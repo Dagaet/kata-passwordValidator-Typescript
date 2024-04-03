a90fb09c00fc917ac772905c7be524b5c68ab57c
Dagaet <alojera15@gmail.com>
2024-04-03 12:19:23 +0100

    Test(Green): Helpers should return true when it contains an upperCase

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,6 +2,12 @@ function hasMoreThanEightCharacters(password:string) {
     return password.length >= 8
 }
 
+function containsAnUppercase(password:string) {
+    const rExp : RegExp = /[A-Z]+/;
+    return rExp.test(password)
+}
+
 export const helpers = {
-    hasMoreThanEightCharacters
+    hasMoreThanEightCharacters,
+    containsAnUppercase
 }
