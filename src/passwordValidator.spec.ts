6c4198157b1c44d6305f399be8ea7de55a8781b9
ravel01106 <ravello2002@gmail.com>
2024-04-08 12:27:38 +0100

    fix: tests

--- src/passwordValidator.spec.ts
+++ src/passwordValidator.spec.ts
@@ -1,14 +1,48 @@
 import { describe, it, expect } from "vitest"
-import { PasswordValidator, } from "./passwordValidator.js"
+import { PasswordValidator } from "./passwordValidator.js"
 
-// Should return true when password meets the conditions
-// Should return false when password doesn't meet the conditions
+// Permit a password when meet all the conditions
+// Not permit a password when it doesnt have more than eight characters
+// Not permit a password when it doesnt have uppercase
+// Not permit a password when it doesnt have lowercase
+// Not permit a password when it doesnt have a number
+// Not permit a password when it doesnt have underscore
 
 describe("PasswordValidator should", () => {
-    it("Permit a password when meet all the conditions", () => {
+  it("Permit a password when meet all the conditions", () => {
+    const passwordValidator = new PasswordValidator()
+    const result = passwordValidator.isPasswordValid("Manuel_Alejandro1998")
+
+    expect(result).toBe(true)
+  }),
+    it("Not permit a password when it doesnt have more than eight characters", () => {
       const passwordValidator = new PasswordValidator()
-      const result = passwordValidator.isPasswordValid("Manuel_Alejandro1998")
-  
-      expect(result).toBe(true)
+      const result = passwordValidator.isPasswordValid("aaAA_")
+
+      expect(result).toBe(false)
+    }),
+    it("Not permit a password when it doesnt have uppercase", () => {
+      const passwordValidator = new PasswordValidator()
+      const result = passwordValidator.isPasswordValid("manuel_alejandro1998")
+
+      expect(result).toBe(false)
+    }),
+    it("Not permit a password when it doesnt have lowercase", () => {
+      const passwordValidator = new PasswordValidator()
+      const result = passwordValidator.isPasswordValid("MANUEL_ALEJANDRO1998")
+
+      expect(result).toBe(false)
+    }),
+    it("Not permit a password when it doesnt have a number", () => {
+      const passwordValidator = new PasswordValidator()
+      const result = passwordValidator.isPasswordValid("Manuel_Alejandro")
+
+      expect(result).toBe(false)
+    }),
+    it("Not permit a password when it doesnt have underscore", () => {
+      const passwordValidator = new PasswordValidator()
+      const result = passwordValidator.isPasswordValid("ManuelAlejandro1998")
+
+      expect(result).toBe(false)
     })
 })
--- src/passwordValidator.ts
+++ src/passwordValidator.ts
@@ -1,35 +1,34 @@
-import { helpers } from "./helpers.js";
-
 export class PasswordValidator {
-
-  isPasswordValid(password:string) {
-    return this.hasMoreThanEightCharacters(password) && this.containsALowercase(password) && 
-    this.containsANumber(password) && this.containsAnUppercase(password) &&
-    this.containsAnUnderscore(password)
+  isPasswordValid(password: string) {
+    return (
+      this.hasMoreThanEightCharacters(password) &&
+      this.containsALowercase(password) &&
+      this.containsANumber(password) &&
+      this.containsAnUppercase(password) &&
+      this.containsAnUnderscore(password)
+    )
   }
 
-  private hasMoreThanEightCharacters(password:string) {
+  private hasMoreThanEightCharacters(password: string) {
     return password.length >= 8
-}
+  }
 
-private containsAnUppercase(password:string) {
-    const rExp : RegExp = /[A-Z]+/;
+  private containsAnUppercase(password: string) {
+    const rExp: RegExp = /[A-Z]+/
     return rExp.test(password)
-}
+  }
 
-private containsALowercase(password:string){
-    const rExp : RegExp = /[a-z]+/;
+  private containsALowercase(password: string) {
+    const rExp: RegExp = /[a-z]+/
     return rExp.test(password)
-}
+  }
 
-private containsANumber(password:string) {
-    const rExp : RegExp = /[0-9]+/;
+  private containsANumber(password: string) {
+    const rExp: RegExp = /[0-9]+/
     return rExp.test(password)
-}
+  }
 
-private containsAnUnderscore(password:string) {
+  private containsAnUnderscore(password: string) {
     return password.includes("_")
+  }
 }
-}
-
-
