c212e6c5670a8a890baacc3dc639e53cba841bf4
Dagaet <alojera15@gmail.com>
2024-04-11 12:40:06 +0100

    refactor: added enum to factory pattern

--- src/factory/passwordValidatorFactory.ts
+++ src/factory/passwordValidatorFactory.ts
@@ -1,17 +1,18 @@
-import { IPasswordValidator } from "../interfaces/IPasswordValidator.js";
-import { PasswordValidator, PasswordValidator2, PasswordValidator3 } from "../passwordValidator.js";
+import { IPasswordValidator } from "../interfaces/IPasswordValidator.js"
+import { PasswordValidatorTypes } from "../models/PasswordValidatorTypes.js"
+import { PasswordValidator, PasswordValidator2, PasswordValidator3 } from "../passwordValidator.js"
 
 export class PasswordValidatorFactory {
-    getPasswordValidator(passwordValidator: String): IPasswordValidator{
-        switch (passwordValidator) {
-            case "1":
-                return new PasswordValidator();
-            case "2":
-                return new PasswordValidator2();
-            case "3":
-                return new PasswordValidator3();
-            default:
-                throw new Error("Error")
-        }
+  getPasswordValidator(passwordValidator: PasswordValidatorTypes): IPasswordValidator {
+    switch (passwordValidator) {
+      case PasswordValidatorTypes.VALIDATOR_ONE:
+        return new PasswordValidator()
+      case PasswordValidatorTypes.VALIDATOR_TWO:
+        return new PasswordValidator2()
+      case PasswordValidatorTypes.VALIDATOR_THREE:
+        return new PasswordValidator3()
+      default:
+        throw new Error("Error")
     }
+  }
 }
--- src/test/passwordValidator.spec.ts
+++ src/test/passwordValidator.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest"
-import { PasswordValidator, PasswordValidator2, PasswordValidator3 } from "../passwordValidator.js"
 import { PasswordValidatorFactory } from "../factory/passwordValidatorFactory.js"
+import { PasswordValidatorTypes } from "../models/PasswordValidatorTypes.js"
 
 // Permit a password when meet all the conditions
 // Not permit a password when it doesnt have more than eight characters
@@ -12,30 +12,40 @@ let factoryPasswordValidator = new PasswordValidatorFactory()
 
 describe("PasswordValidator should", () => {
   it("Permit a password when meet all the conditions", () => {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
     const result = passwordValidator.isPasswordValid("Manuel_Alejandro1998")
 
     expect(result).toBe(true)
   }),
     it("Not permit a password when it doesnt have more than eight characters", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-      expect(() => passwordValidator.isPasswordValid("Aaa_1")).toThrowError(/^Error, password:\ndoes not have the required length$/)
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+      expect(() => passwordValidator.isPasswordValid("Aaa_1")).toThrowError(
+        /^Error, password:\ndoes not have the required length$/,
+      )
     }),
     it("Not permit a password when it doesnt have uppercase", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-      expect(() => passwordValidator.isPasswordValid("manuel_alejandro1998")).toThrowError(/^Error, password:\ndoes not have uppercase$/)
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+      expect(() => passwordValidator.isPasswordValid("manuel_alejandro1998")).toThrowError(
+        /^Error, password:\ndoes not have uppercase$/,
+      )
     }),
     it("Not permit a password when it doesnt have lowercase", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-      expect(() => passwordValidator.isPasswordValid("MANUEL_ALEJANDRO1998")).toThrowError(/^Error, password:\ndoes not have lowercase$/)
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+      expect(() => passwordValidator.isPasswordValid("MANUEL_ALEJANDRO1998")).toThrowError(
+        /^Error, password:\ndoes not have lowercase$/,
+      )
     }),
     it("Not permit a password when it doesnt have a number", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-      expect(() => passwordValidator.isPasswordValid("Manuel_Alejandro")).toThrowError(/^Error, password:\ndoes not have a number$/)
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+      expect(() => passwordValidator.isPasswordValid("Manuel_Alejandro")).toThrowError(
+        /^Error, password:\ndoes not have a number$/,
+      )
     }),
     it("Not permit a password when it doesnt have underscore", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-      expect(() => passwordValidator.isPasswordValid("ManuelAlejandro1998")).toThrowError(/^Error, password:\ndoes not have an underscore$/)
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+      expect(() => passwordValidator.isPasswordValid("ManuelAlejandro1998")).toThrowError(
+        /^Error, password:\ndoes not have an underscore$/,
+      )
     })
 })
 
@@ -47,31 +57,31 @@ describe("PasswordValidator should", () => {
 
 describe("PasswordValidator2 should ", () => {
   it("Not permit a password when it doesnt have more than six characters", () => {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("2")
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_TWO)
     const result = passwordValidator.isPasswordValid("M987a")
 
     expect(result).toBe(false)
   }),
     it("Not permit a password when it doesnt have uppercase", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("2")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_TWO)
       const result = passwordValidator.isPasswordValid("a987a98e")
 
       expect(result).toBe(false)
     }),
     it("Not permit a password when it doesnt have lowercase", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("2")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_TWO)
       const result = passwordValidator.isPasswordValid("A987A98E")
 
       expect(result).toBe(false)
     }),
     it("Not permit a password when it doesnt have a number", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("2")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_TWO)
       const result = passwordValidator.isPasswordValid("ABCeABCE")
 
       expect(result).toBe(false)
     }),
     it("Permit a password when meet all conditions", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("2")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_TWO)
       const result = passwordValidator.isPasswordValid("ManuelAlejandro1998")
 
       expect(result).toBe(true)
@@ -80,37 +90,36 @@ describe("PasswordValidator2 should ", () => {
 
 describe("PasswordValidator3 should ", () => {
   it("Not permit a password when it doesnt have more than sixteen characters", () => {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("3")
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_THREE)
     const result = passwordValidator.isPasswordValid("Ma_")
 
     expect(result).toBe(false)
   }),
     it("Not permit a password when it doesnt have uppercase", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("3")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_THREE)
       const result = passwordValidator.isPasswordValid("unacontrasenialarga_")
 
       expect(result).toBe(false)
     }),
     it("Not permit a password when it doesnt have lowercase", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("3")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_THREE)
       const result = passwordValidator.isPasswordValid("UNACONTRASENIALARGA_")
 
       expect(result).toBe(false)
     }),
     it("Not permit a password when it doesnt have underscore", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("3")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_THREE)
       const result = passwordValidator.isPasswordValid("unacontraseniaLarga")
 
       expect(result).toBe(false)
     }),
     it("Permit a password when meet all conditions", () => {
-      const passwordValidator = factoryPasswordValidator.getPasswordValidator("3")
+      const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_THREE)
       const result = passwordValidator.isPasswordValid("unacontraseniaLarga_")
 
       expect(result).toBe(true)
     })
 })
-
 
 // Password should throw error when given less characters than needed
 // Password should throw error when it has no uppercase
@@ -122,22 +131,30 @@ describe("PasswordValidator3 should ", () => {
 
 describe("PasswordValidator should throw error", () => {
   it("When given less characters than needed", () => {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-    expect(() => passwordValidator.isPasswordValid("Aaa_1")).toThrowError(/^Error, password:\ndoes not have the required length$/)
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+    expect(() => passwordValidator.isPasswordValid("Aaa_1")).toThrowError(
+      /^Error, password:\ndoes not have the required length$/,
+    )
   })
 
   it("When it has no uppercase", () => {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-    expect(() => passwordValidator.isPasswordValid("manuel_alejandro1998")).toThrowError(/^Error, password:\ndoes not have uppercase$/)
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+    expect(() => passwordValidator.isPasswordValid("manuel_alejandro1998")).toThrowError(
+      /^Error, password:\ndoes not have uppercase$/,
+    )
   })
 
-  it("When fail various conditions", ()=> {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-    expect(() => passwordValidator.isPasswordValid("manuel_alejandro")).toThrowError(/^Error, password:\ndoes not have uppercase\ndoes not have a number$/)
+  it("When fail various conditions", () => {
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+    expect(() => passwordValidator.isPasswordValid("manuel_alejandro")).toThrowError(
+      /^Error, password:\ndoes not have uppercase\ndoes not have a number$/,
+    )
   })
 
   it("when doesn't meet no condition", () => {
-    const passwordValidator = factoryPasswordValidator.getPasswordValidator("1")
-    expect(() => passwordValidator.isPasswordValid("")).toThrowError(/^Error, password:\ndoes not have the required length\ndoes not have lowercase\ndoes not have uppercase\ndoes not have a number\ndoes not have an underscore$/)
+    const passwordValidator = factoryPasswordValidator.getPasswordValidator(PasswordValidatorTypes.VALIDATOR_ONE)
+    expect(() => passwordValidator.isPasswordValid("")).toThrowError(
+      /^Error, password:\ndoes not have the required length\ndoes not have lowercase\ndoes not have uppercase\ndoes not have a number\ndoes not have an underscore$/,
+    )
   })
 })
